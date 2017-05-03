46c001a4f7d89e80d67c564cb7665c97b5e4f3fd
Alec Robins <arobins@hubspot.com>
2017-05-02 20:33:00 -0400

    Move files from Assignmetn4 into Assingment 5

---
+++ FinalProject/FinalProject/SimulationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  Assignment4
+//
+//  Created by Van Simmons on 1/15/17.
+//  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
+//
+
+import UIKit
+
+class SimulationViewController: UIViewController, EngineDelegate {
+
+    @IBOutlet weak var gridView: GridView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        StandardEngine.sharedEngine.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func step(_ sender: UIButton) {
+        StandardEngine.sharedEngine.next()
+    }
+    
+    func engineDidUpdate(withGrid: GridProtocol) {
+        gridView.setNeedsDisplay()
+    }
+}
+
